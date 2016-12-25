f1400a6ddb14fb28d9496f4fb5b3aa1e94a578ac
Jason <jason.barnabe@gmail.com>
2016-12-24 22:13:11 -0600

    Switch from PHP_SELF to SCRIPT_NAME for Vanilla 2.3

--- prepend.php
+++ prepend.php
@@ -1,5 +1,5 @@
 <?php
 if(array_key_exists('locale', $_GET)){
-  $_SERVER['PHP_SELF'] = '/'.$_GET['locale'].$_SERVER['PHP_SELF'];
+  $_SERVER['SCRIPT_NAME'] = '/'.$_GET['locale'].$_SERVER['SCRIPT_NAME'];
 }
 
